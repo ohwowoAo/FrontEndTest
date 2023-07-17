b81d618d6a29637fec25647bee8f1a01508657c8
ohwowoAo <sbjdnm@gmail.com>
2023-07-17 15:44:01 +0900

    Feat : 테스트 과제 작업

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
 import React from "react";
 import styled from "styled-components";
 
+const borderStyle = "3px solid #000";
+
 const App = () => {
   return (
     <Wrapper>
       <Contents>
-        <Box className="white"></Box>
-        <Box className="pink"></Box>
-        <Box className="black"></Box>
-        <Box className="black noBorR"></Box>
-        {/* <WhiteBox></WhiteBox>
-        <PinkBox></PinkBox>
-        <BlackBox></BlackBox>
-        <BlackBox></BlackBox> */}
-        <Box className="pink"></Box>
-        <Box className="green noBorB noBorR"></Box>
-        <Box className="green noBorB"></Box>
-        <Box className="skyblue noBorR"></Box>
-        <Box className="black"></Box>
-        <Box className="green noBorR"></Box>
-        <Box className="green"></Box>
-        <Box className="yellow noBorR noBorB"></Box>
-        <Box className="black noBorB"></Box>
-        <Box className="black noBorB"></Box>
-        <Box className="yellow noBorR noBorB"></Box>
-        <Box className="yellow noBorR noBorB"></Box>
+        <BoxWrap className="noBorR noBorB">
+          <Box className="white"></Box>
+          <Box className="pink noBorR"></Box>
+          <Box className="pink noBorB"></Box>
+        </BoxWrap>
+        <BoxWrap className="noBorB">
+          <Box className="black"></Box>
+          <Box className="black noBorR"></Box>
+          <Box className="black"></Box>
+          <Box className="skyblue noBorB noBorR"></Box>
+          <ChildBox className="blue"></ChildBox>
+        </BoxWrap>
+        <BoxWrap className="black noBorR">
+          <ChildBox className="white"></ChildBox>
+        </BoxWrap>
+        <BoxWrap className="yellow"></BoxWrap>
+        <CenterBox>
+          <CenterRedBox></CenterRedBox>
+          <CenterWhiteBox></CenterWhiteBox>
+        </CenterBox>
       </Contents>
     </Wrapper>
   );
@@ -34,46 +36,95 @@ export default App;
 
 const Wrapper = styled.div``;
 const Contents = styled.div`
+  position: relative;
   width: 600px;
   height: 600px;
   margin: 10px;
-  border: 3px solid #000;
   font-size: 0;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(2, 1fr);
 `;
-const Box = styled.div`
-  width: calc(100% / 4);
-  height: calc(100% / 4);
-  display: inline-block;
+const CenterBox = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: calc(100% / 2 - 3px);
+  height: calc(100% / 2 - 3px);
+  border: ${borderStyle};
+  background-color: #40fe00;
+`;
+const CenterRedBox = styled.div`
+  position: absolute;
+  bottom: 70px;
+  left: 0;
+  z-index: 1;
+  width: 150px;
+  height: 150px;
   box-sizing: border-box;
-  background-color: ${(props) =>
-    props.className.includes("white")
-      ? "#fff"
-      : props.className.includes("black")
-      ? "#000"
-      : props.className.includes("pink")
-      ? "#f91aff"
-      : props.className.includes("green")
-      ? "#40fe00"
-      : props.className.includes("skyblue")
-      ? "#3fffff"
-      : props.className.includes("yellow")
-      ? "#fffe00"
-      : "initial"};
-  /* border-right: ${(props) =>
-    props.className.includes("borR") ? "2px solid #000" : "initial"}; */
-  border-right: ${(props) =>
-    props.className.includes("noBorR") ? "initial" : "3px solid #000"};
-  border-bottom: ${(props) =>
-    props.className.includes("noBorB") ? "initial" : "3px solid #000"};
+  border-top: ${borderStyle};
+  border-right: ${borderStyle};
+  background-color: #f90000;
 `;
-// const WhiteBox = styled.div`
-//   background-color: #fff;
-// `;
-// const PinkBox = styled.div`
-//   background-color: #f91aff;
-// `;
-// const BlackBox = styled.div`
-//   width: calc(100% / 4);
-//   height: calc(100% / 4);
-//   background-color: #000;
-// `;
+const CenterWhiteBox = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 73px;
+  z-index: 1;
+  width: 150px;
+  height: 150px;
+  box-sizing: border-box;
+  border: ${borderStyle};
+  border-bottom: none;
+  background-color: #fff;
+`;
+const BoxWrap = styled.div`
+  position: relative;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(2, 1fr);
+  box-sizing: border-box;
+  border: ${borderStyle};
+  border-right: ${(props) =>
+    props.className && props.className.includes("noBorR")
+      ? "initial"
+      : borderStyle};
+  border-bottom: ${(props) =>
+    props.className && props.className.includes("noBorB")
+      ? "initial"
+      : borderStyle};
+  background-color: ${(props) => getBackgroundColor(props.className)};
+`;
+
+const Box = styled.div`
+  box-sizing: border-box;
+  border-right: ${(props) =>
+    props.className.includes("noBorR") ? "initial" : borderStyle};
+  border-bottom: ${(props) =>
+    props.className.includes("noBorB") ? "initial" : borderStyle};
+  background-color: ${(props) => getBackgroundColor(props.className)};
+`;
+
+const ChildBox = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 1;
+  width: 150px;
+  height: 150px;
+  box-sizing: border-box;
+  border: ${borderStyle};
+  background-color: ${(props) => getBackgroundColor(props.className)};
+`;
+const getBackgroundColor = (className) => {
+  if (className.includes("white")) return "#fff";
+  if (className.includes("black")) return "#000";
+  if (className.includes("pink")) return "#f91aff";
+  if (className.includes("green")) return "#40fe00";
+  if (className.includes("skyblue")) return "#3fffff";
+  if (className.includes("yellow")) return "#fffe00";
+  if (className.includes("blue")) return "#001fff";
+  return "initial";
+};
