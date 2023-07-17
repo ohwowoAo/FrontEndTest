23c0e5f4c975c74ef663a880bda848f6cbf026cd
ohwowoAo <sbjdnm@gmail.com>
2023-07-17 14:12:54 +0900

    Feat : 낱개 box로 구성하는 방법

--- src/App.js
+++ src/App.js
@@ -8,23 +8,23 @@ const App = () => {
         <Box className="white"></Box>
         <Box className="pink"></Box>
         <Box className="black"></Box>
-        <Box className="black"></Box>
+        <Box className="black noBorR"></Box>
         {/* <WhiteBox></WhiteBox>
         <PinkBox></PinkBox>
         <BlackBox></BlackBox>
         <BlackBox></BlackBox> */}
         <Box className="pink"></Box>
+        <Box className="green noBorB noBorR"></Box>
+        <Box className="green noBorB"></Box>
+        <Box className="skyblue noBorR"></Box>
+        <Box className="black"></Box>
+        <Box className="green noBorR"></Box>
         <Box className="green"></Box>
-        <Box className="green"></Box>
-        <Box className="skyblue"></Box>
-        <Box className="black"></Box>
-        <Box className="black"></Box>
-        <Box className="yellow"></Box>
-        <Box className="yellow"></Box>
-        <Box className="black"></Box>
-        <Box className="black"></Box>
-        <Box className="yellow"></Box>
-        <Box className="yellow"></Box>
+        <Box className="yellow noBorR noBorB"></Box>
+        <Box className="black noBorB"></Box>
+        <Box className="black noBorB"></Box>
+        <Box className="yellow noBorR noBorB"></Box>
+        <Box className="yellow noBorR noBorB"></Box>
       </Contents>
     </Wrapper>
   );
@@ -37,24 +37,34 @@ const Contents = styled.div`
   width: 600px;
   height: 600px;
   margin: 10px;
-  border: 2px solid #000;
+  border: 3px solid #000;
+  font-size: 0;
 `;
 const Box = styled.div`
   width: calc(100% / 4);
   height: calc(100% / 4);
   display: inline-block;
+  box-sizing: border-box;
   background-color: ${(props) =>
-    props.className === "black"
+    props.className.includes("white")
+      ? "#fff"
+      : props.className.includes("black")
       ? "#000"
-      : props.className === "pink"
+      : props.className.includes("pink")
       ? "#f91aff"
-      : props.className === "green"
+      : props.className.includes("green")
       ? "#40fe00"
-      : props.className === "skyblue"
+      : props.className.includes("skyblue")
       ? "#3fffff"
-      : props.className === "yellow"
+      : props.className.includes("yellow")
       ? "#fffe00"
       : "initial"};
+  /* border-right: ${(props) =>
+    props.className.includes("borR") ? "2px solid #000" : "initial"}; */
+  border-right: ${(props) =>
+    props.className.includes("noBorR") ? "initial" : "3px solid #000"};
+  border-bottom: ${(props) =>
+    props.className.includes("noBorB") ? "initial" : "3px solid #000"};
 `;
 // const WhiteBox = styled.div`
 //   background-color: #fff;
